94fc4d135a509aeca23c7d49b4ecc442954868ef
hayward-saasquatch <hayward@referralsaasquatch.com>
2017-03-21 14:47:52 -0700

    HP: change engagementMedium to a variable

--- Tealium/squatch-dynamic-tag.js
+++ Tealium/squatch-dynamic-tag.js
@@ -5,7 +5,7 @@ window.squatch.ready(function(){
       tenantAlias: 'test_amtbexleynygg'
   });
   var initObj = {
-    engagementMedium: b["engagementMedium"],
+    engagementMedium: engagementMedium,
     widgetType: 'REFERRER_WIDGET'
   };
 
